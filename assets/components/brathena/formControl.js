da2d8cf3e5a3525156affcad5eb853a630f65391
Rafael Gorges <rafaelgorges@outlook.com>
2017-08-27 14:13:45 -0300

    Criada a página de cadastro. Criado controlador para cadastro e botão de resetar informações do formulário

--- assets/components/brathena/formControl.js
+++ assets/components/brathena/formControl.js
@@ -4,7 +4,7 @@ var brApp = angular.module('brApp', []);
 // Criação de um controlador para o angular (controller)
 brApp.controller('formController', function($scope) {
 		
-	// Função de envio (submit) do formulário após a validação ocorre	
+	// Função de envio (submit) do formulário de login após a validação
 	$scope.submitLogin = function() {
 
 		// Verificação para validação do formulário
@@ -14,4 +14,21 @@ brApp.controller('formController', function($scope) {
 			alert('Erro ao validar o login!');
 		}
 	};
+	// Função de envio (submit) do formulário de registro após a validação	
+	$scope.submitReg = function() {
+
+		// Verificação para validação do formulário
+		if ($scope.regForm.$valid) {
+			alert('Cadastro realizado com sucesso!');
+		} else {
+			alert('Erro ao validar informações do cadastro!');
+		}
+	};
+
+	$scope.reset = function() {
+		$scope.user = angular.copy($scope.master);
+	};
+
+	$scope.reset();
+
 });
