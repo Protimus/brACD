6de081bfdf52e0145f725505fa534459c9570821
Rafael Gorges <rafaelgorges@outlook.com>
2017-08-31 15:31:02 -0300

    Criada tabela para bugs, padronizações e adição de links ao cms principal

--- app/sql/main.sql
+++ app/sql/main.sql
@@ -3,22 +3,22 @@ CREATE DATABASE bracd;
 use bracd;
 
 /** Cria a tabela de contas **/
-CREATE TABLE IF NOT EXISTS `acc` (
-	`acc_id`	int(10) unsigned NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `user` (
+	`uid`	int(10) unsigned NOT NULL AUTO_INCREMENT,
     `user`	varchar(25) COLLATE utf8_unicode_ci NOT NULL,
     `mail`	varchar(30)	NOT NULL,
     `pass`	varchar(25) COLLATE utf8_unicode_ci NOT NULL,
-    PRIMARY KEY	(`account_id`),
+    PRIMARY KEY	(`uid`),
     UNIQUE KEY `user` (`user`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1;
 
 /** Cria a tabela de grupos **/
-CREATE TABLE IF NOT EXISTS `acc_group` (
-	`acc_id`	int(10) unsigned NOT NULL,
-	`group_id` 	int(10) unsigned NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `user_group` (
+	`uid`	int(10) unsigned NOT NULL,
+	`gid` 	int(10) unsigned NOT NULL AUTO_INCREMENT,
 	`title`	varchar(20)	DEFAULT 'Membro',
-	PRIMARY KEY (`group_id`),
-	FOREIGN KEY (`acc_id`) REFERENCES `acc`(`acc_id`)
+	PRIMARY KEY (`gid`),
+	FOREIGN KEY (`uid`) REFERENCES `user`(`uid`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1;
 
 /** Cria a tabela de traduções 
@@ -27,14 +27,23 @@ CREATE TABLE IF NOT EXISTS `acc_group` (
 - Status 2 = Revisando tradução
 - Status 3 = Conclusão de tradução **/
 CREATE TABLE IF NOT EXISTS `translation` (
-	`trans_id`	int(10) unsigned NOT NULL AUTO_INCREMENT,
-	`acc_id` 	int(10) unsigned NOT NULL,
-	`trans_status` smallint(2) NOT NULL,
-	`trans_name`	varchar(30) NOT NULL,
-	PRIMARY KEY	(`trans_id`),
-	FOREIGN KEY (`account_id`) REFERENCES `acc`(`account_id`)
+	`tid`	int(10) unsigned NOT NULL AUTO_INCREMENT,
+	`uid` 	int(10) unsigned NOT NULL,
+	`tstatus` smallint(2) NOT NULL,
+	`tname`	varchar(30) NOT NULL,
+	PRIMARY KEY	(`tid`),
+	FOREIGN KEY (`uid`) REFERENCES `user`(`uid`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1;
+
+CREATE TABLE IF NOT EXISTS `bugs` (
+	`big`	int(10) unsigned NOT NULL AUTO_INCREMENT,
+	`btitle`	varchar(30) NOT NULL, 
+	`bstatus`	smallint(2) NOT NULL,
+	`bauthor`	int(10) unsigned NOT NULL,
+	PRIMARY KEY (`big`),
+	FOREIGN KEY (`bauthor`) REFERENCES `user`(`uid`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1;
 
 /* População de usuário padrão para conta de administrador */
-INSERT INTO acc (acc_id, user, mail, pass) VALUES ('1','root','admin@dev.brathena.org','toor');
-INSERT INTO acc_group(acc_id, group_id, title) VALUES ('1','1','Administrador');
+INSERT INTO user(uid, user, mail, pass) VALUES ('1','root','admin@dev.brathena.org','toor');
+INSERT INTO user_group(uid, gid, title) VALUES ('1','1','Administrador');
