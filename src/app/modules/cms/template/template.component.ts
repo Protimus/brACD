fd9ee13f8fec75c7fc73c5f7bcd10e47cc149c4b
Marcus Cartágenes <mvcartagenes@gmail.com>
2018-02-17 13:05:44 -0300

    Template

--- src/app/modules/cms/template/template.component.ts
+++ src/app/modules/cms/template/template.component.ts
@@ -7,9 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TemplateComponent implements OnInit {
 
+  year:number;
   constructor() { }
 
   ngOnInit() {
+    this.year = new Date().getFullYear();
   }
 
 }
